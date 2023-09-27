972569bc26d9f60d11f04a819a13b8028f1ce2bd
Satyaprakash Das <satyadasdipu5@gmail.com>
2023-09-28 00:41:36 +0530

    Navbar Created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,32 @@
+import { hamburger } from "../assets/Icons";
+import { HeaderLogo } from "../assets/images";
+
+import {navLinks} from "../Constants"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="padding-x absolute py-8 z-10 w-full " >
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={HeaderLogo} alt="Logo" width={130} height={30} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden ">
+        {
+          navLinks.map((item)=>(
+            <li key={item.label}>
+              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
+            </li>
+          ))
+        }
+        <li className="font-montserrat leading-normal text-lg text-black">Sign Up</li>
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
